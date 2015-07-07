1ec0565f6c92de3cbfd6a1614d30330f09e9d80c
Sangevan <prettysangevan@hotmail.com>
2015-07-07 11:58:34 -0700

    add code

---
+++ angle_subtraction.m
@@ -0,0 +1,22 @@
+function [thetaResult] = angle_subtraction(theta1, theta2)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+n = length(theta2);
+thetaResult = zeros(n,1);
+
+if length(theta1) == 1
+    theta1 = theta1*ones(n,1);
+end
+
+for i = 1:n
+    if theta1(i) - theta2(i) <= -180
+        thetaResult(i) = -(- 180 - theta1(i)) + (180 - theta2(i));
+    elseif theta1(i) - theta2(i) >= 180
+        thetaResult(i) = -((180 - theta1(i)) - (- 180 - theta2(i)));
+    else
+        thetaResult(i) = theta1(i) - theta2(i);
+    end
+end
+
+end
