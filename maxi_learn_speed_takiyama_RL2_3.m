86d0a18113c7607206dca270d555116e87eeb2f7
Sangevan <prettysangevan@hotmail.com>
2015-07-23 11:36:19 -0700

    Z(m,n)

--- maxi_learn_speed_takiyama_RL2_3.m
+++ maxi_learn_speed_takiyama_RL2_3.m
@@ -10,20 +10,20 @@ K = 10; %number of position of theta in the circle (real number is K+1)
 thetak = (2*pi)/K:(2*pi)/K:2*pi;
 randomOrder = randi(K,T);
 mOutput = 2; %number of outputs
-nInput = 2; %number of neurons
+nInput = 4; %number of neurons
 phiInput = 30; % angle of perturbation
 
-nTrials = 20000;
+nTrials = 10000;
 phi = zeros(1,nInput);
-v = zeros(1,nTrials);
+
 
 % Connectivity (weight) matrix (W)
 Z=zeros(mOutput, nInput);
 %E=zeros(1,nTrials); %Faux, car E un reel
 W1 = rand(nInput, mOutput);
-W2= rand(nInput, mOutput);
+
 V1 = rand(nInput, mOutput);
-V2= rand(nInput, mOutput);
+
 Wthres1 = zeros(nInput,1);
 Vthres1= zeros(nInput,1);
 expectR1= zeros(nInput,1);
@@ -31,8 +31,8 @@ O1= zeros(nInput,1);
 A1= zeros(nInput,1);
 y= zeros(nInput,1);
 omega1= zeros(nInput,1);
-Wthres2 = zeros(nInput,1);
-Vthres2= zeros(nInput,1);
+
+
 deltaW1= zeros(nInput,1);
 deltaV1= zeros(nInput,1);
 %R = [cos(phiInput),-sin(phiInput);sin(phiInput),cos(phiInput)];
@@ -45,9 +45,8 @@ lambda= NaN(1,mOutput);
 desMagnitude=1;
 %xOutput = zeros(mOutput,1);
 xOutput = [0,0]';
-U1 = [0,0]';
-U2 = [0,0]';
-B=1;
+
+
 rewardThreshold=2;
 rewardThreshold1=2*nInput;
 rewardThreshold=4*nInput-4;
@@ -57,11 +56,12 @@ alpha2=1/4;
 beta2=1/2;
 E = zeros(nTrials,1);
 r1=zeros(nInput,1);
-%for k = 2:nInput
-%   phi(k)= rand;    % !!!!!Angle!!!this phi concerns about tge foce direction(FD)
-%  Y = (1/nInput)*[cos(phi(k)*2*pi);sin(phi(k)*2*pi)]';
-% Z(:,k)=Y;
-%end
+
+for k = 2:nInput
+   phi(k)= rand;    % !!!!!Angle!!!this phi concerns about tge foce direction(FD)
+ Y = (1/nInput)*[cos(phi(k)*2*pi);sin(phi(k)*2*pi)]';
+Z(:,k)=Y;
+end
 
 
 for j = 1:T
@@ -80,23 +80,15 @@ for j = 1:T
             y(p)=(2./(1+ exp(-A1(p))))-1;
             
             
-            %      mu2=W1*tTarget+Wthres2;   %first element of Y2
-            %    expectR2=V1*tTarget+Vthres2;
-            %     O2=[(1-expectR2)/2 0];
-            %     omega2=max(O2);
-            %     A2=normrnd(mu2,omega2);
-            %     y(2)=(2./(1+ exp(-A2)))-1;
+           
         end
         %determination of X the output
         xOutput=Z*y;
-        % xOutput(1)=U1(1);
-        % U2=Z*y(2);
-        % xOutput(2)=U2(2);
-        %RL for the first element of the output
+  
+        %RL forthe output
         e = (1/2)*(tTarget-xOutput)'*(tTarget-xOutput);
         
-        %RL for the second element of the output
-        % e2 = (1/2)*((tTarget(2))-Z(2)*y(2))^2;
+        
         if e<=1
             r= max(0,(rewardThreshold - e)/rewardThreshold);
         else
